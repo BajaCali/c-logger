150151bec390cf2718c48a9ae723d3ed5ec4874f
BajaCali <michaelnemec47@gmail.com>
2018-05-06 00:09:53 +0200

    Add macros for other priorities

--- logger.h
+++ logger.h
@@ -7,8 +7,10 @@
 #include <syslog.h>
 
 #ifndef LOG_LEVEL
-#define LOG_LEVEL LOG_EMERG
+#define LOG_LEVEL LOG_DEBUG
 #endif
+
+
 
 #ifndef _DISABLE_LOGGER
 #define _DISABLE_LOGGER 0
@@ -23,16 +25,48 @@
     } while(0)
 
 
-#if LOG_LEVEL >= LOG_EMERG && !_DISABLE_LOGGER 
+#if LOG_LEVEL >= LOG_EMERG && !_DISABLE_LOGGER
 #define LOGF_EMERG(f_str, ...) LOG_MSG("emerg", f_str, ##__VA_ARGS__)
 #else
-#define LOGF_EMERG(f_str, ...) 
+#define LOGF_EMERG(f_str, ...)
+#endif
+
+#if LOG_LEVEL >= LOG_ALERT && !_DISABLE_LOGGER
+#define LOGF_ALERT(f_str, ...) LOG_MSG("alert", f_str, ##__VA_ARGS__)
+#else
+#define LOGF_ALERT(f_str, ...)
+#endif
+
+#if LOG_LEVEL >= LOG_CRIT && !_DISABLE_LOGGER
+#define LOGF_CRIT(f_str, ...) LOG_MSG("crit", f_str, ##__VA_ARGS__)
+#else
+#define LOGF_CRIT(f_str, ...)
+#endif
+
+#if LOG_LEVEL >= LOG_ERR && !_DISABLE_LOGGER
+#define LOGF_ERR(f_str, ...) LOG_MSG("err", f_str, ##__VA_ARGS__)
+#else
+#define LOGF_ERR(f_str, ...)
+#endif
+
+#if LOG_LEVEL >= LOG_WARNING && !_DISABLE_LOGGER
+#define LOGF_WARINING(f_str, ...) LOG_MSG("warning", f_str, ##__VA_ARGS__)
+#else
+#define LOGF_WARINING(f_str, ...)
+#endif
+
+#if LOG_LEVEL >= LOG_NOTICE && !_DISABLE_LOGGER
+#define LOGF_NOTICE(f_str, ...) LOG_MSG("notice", f_str, ##__VA_ARGS__)
+#else
+#define LOGF_NOTICE(f_str, ...)
 #endif
 
 #if LOG_LEVEL >= LOG_INFO && !_DISABLE_LOGGER
 #define LOGF_INFO(f_str, ...) LOG_MSG("info", f_str, ##__VA_ARGS__)
 #else
-#define LOGF_INFO(f_str, ...) 
+#define LOGF_INFO(f_str, ...)
 #endif
 
-#endif
+
+
+#endif // LOGGER_H
